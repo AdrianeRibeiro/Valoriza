356e88e18a9c16933410b1e3b7b938ca1f0e3bb4
Adriane Ribeiro <adrianeribeiro94@gmail.com>
2021-09-11 15:57:54 -0300

    adicionando estrutura para fazer elogios

--- src/routes.ts
+++ src/routes.ts
@@ -2,12 +2,18 @@ import { request, Router } from "express";
 import { CreateUserController } from "./controllers/CreateUserController";
 import { CreateTagController } from "./controllers/CreateTagController";
 import { ensureAdmin } from "./middlewares/ensureAdmin";
+import { AutheticateUserController } from "./controllers/AuthenticateUserController";
+import { CreateComplimentController } from "./controllers/CreateComplimentController";
 
 const router = Router();
 const createUserController = new CreateUserController()
 const createTagController = new CreateTagController()
+const authenticateUserController = new AutheticateUserController()
+const createComplimentController = new CreateComplimentController()
 
 router.post("/tags", ensureAdmin, createTagController.handle)
 router.post("/users", createUserController.handle)
+router.post("/login", authenticateUserController.handle)
+router.post("/compliments", createComplimentController.handle)
 
 export { router }
