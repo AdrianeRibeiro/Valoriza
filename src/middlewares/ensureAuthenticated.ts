b858c97af8c25de5cf942cf03a82250d8ef2fc45
Adriane Ribeiro <adrianeribeiro94@gmail.com>
2021-09-20 20:56:13 -0300

    adicionando autenticaçao

---
+++ src/middlewares/ensureAuthenticated.ts
@@ -0,0 +1,27 @@
+import { Request, Response, NextFunction } from "express"
+import { verify } from "jsonwebtoken"
+
+interface IPayLoad {
+  sub: string;
+}
+
+export function ensureAuthenticated(request: Request, response: Response, next: NextFunction) {
+  const authToken = request.headers.authorization
+
+  if(!authToken) {
+    return response.status(401).end()
+  }
+
+  const [,token] = authToken.split(" ")
+
+  try {
+    const { sub } = verify(token, "6425ddbf9cd648e1e4d33c4340d3373d") as IPayLoad
+    return next()
+    
+    request.user_id = sub
+  }catch(err) {
+    response.status(401).end()
+  }
+  
+  return next()
+}
--- src/routes.ts
+++ src/routes.ts
@@ -2,6 +2,8 @@ import { request, Router } from "express";
 import { CreateUserController } from "./controllers/CreateUserController";
 import { CreateTagController } from "./controllers/CreateTagController";
 import { ensureAdmin } from "./middlewares/ensureAdmin";
+import { ensureAuthenticated } from "./middlewares/ensureAuthenticated";
+
 import { AutheticateUserController } from "./controllers/AuthenticateUserController";
 import { CreateComplimentController } from "./controllers/CreateComplimentController";
 
@@ -11,9 +13,9 @@ const createTagController = new CreateTagController()
 const authenticateUserController = new AutheticateUserController()
 const createComplimentController = new CreateComplimentController()
 
-router.post("/tags", ensureAdmin, createTagController.handle)
+router.post("/tags", ensureAuthenticated, ensureAdmin, createTagController.handle)
 router.post("/users", createUserController.handle)
 router.post("/login", authenticateUserController.handle)
-router.post("/compliments", createComplimentController.handle)
+router.post("/compliments", ensureAuthenticated, createComplimentController.handle)
 
 export { router }
